ab9ce25ab73000deab1a818b0c770aa970edec10
Walter Betz Jr <129965715+Warbo360@users.noreply.github.com>
2026-06-14 21:26:42 -0500

    split_nodes_delimiter() worked on need to write tests for it next

--- src/md_splitter.py
+++ src/md_splitter.py
@@ -1,20 +1,20 @@
-from enum import Enum
 from textnode import TextNode, TextType
-
-class MDDelimiter(Enum):
-    ITALIC = "_"
-    BOLD = "**"
-    CODE = "`"
 
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
     new_nodes = []
     for old_node in old_nodes:
         if old_node.text_type is not TextType.PLAIN:
             new_nodes.append(old_node)
+
+        temp: list[TextNode] = []
+
         if delimiter in old_node.text:
-            split_node = old_node.text.split(delimiter)
-            if len(split_node) % 2 == 0:
-                raise ValueError(f"{old_node} has odd number of \"{delimiter}\" and thus is not valid markdown")
-                         
-
-
+            split_old_node = old_node.text.split(delimiter)
+            if len(split_old_node) % 2 == 0:
+                raise ValueError(f"{old_node} has unmatched number of delimiter: \"{delimiter}\"")
+            for i in range(len(split_old_node)):
+                if i % 2 == 0:
+                    temp.append(TextNode(split_old_node[i], TextType.PLAIN))
+                temp.append(TextNode(split_old_node[i], text_type))
+        new_nodes.extend(temp)
+    return new_nodes
---
+++ src/test_md_splitter.py
@@ -0,0 +1,5 @@
+import unittest
+from md_splitter import split_nodes_delimiter
+
+class TextSplitNodesDelimiter(unittest.TestCase):
+    pass
