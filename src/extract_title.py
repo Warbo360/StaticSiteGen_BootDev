4800782c560fae977eeebe9ab21756f61f392855
Walter Betz Jr <129965715+Warbo360@users.noreply.github.com>
2026-06-26 16:33:39 -0500

    some more work on generate page

--- src/extract_title.py
+++ src/extract_title.py
@@ -1,6 +1,6 @@
 import re
 
-def extract_title(md: str) -> str | None:
+def extract_title(md: str) -> str:
     header: re.Match[str] | None = re.search(r"# (.+)", md, re.M)
     if not header:
         raise Exception(f"ERROR: \"{md}\" does not contain a \"h1\" header")
