6b8b4255b36e4f56ab4af24226e86ed0d83f8b85
Walter Betz Jr <129965715+Warbo360@users.noreply.github.com>
2026-06-25 20:14:13 -0500

    static to public implemented

--- src/static_to_public.py
+++ src/static_to_public.py
@@ -6,25 +6,22 @@ def static_to_public(src: str, target: str) -> None:
     if os.path.exists(target):
         shutil.rmtree(target)
         os.mkdir(target)
-        print(f"SUCCESS: deleted contents of {target}")
-        copied_file_paths = copy_src(src, target)
-        print(copied_file_paths)
-        for filepath in copied_file_paths:
-            print(filepath.replace(src, target))
-
+        print(f"\nSUCCESS: deleted contents of \"{target}\"")
+        try:
+            copy_src(src, target)
+            print(f"\nSUCCESS: Files copied from \"{src}\" to \"{target}\"")
+        except Exception as e:
+            print(f"An error occured {e}")
     else:
-        print(f"INFO: \"{target}\" directory does not exist... Creating directory")
-        os.mkdir(target)
+        print(f"\nERROR: \"{target}\" directory does not exist... Creating directory")
 
-def copy_src(src: str, target: str) -> list[str]:
-    file_list: list[str] = []
+def copy_src(src: str, target: str) -> None:
     current_dir: list[str] = os.listdir(src)
     for i in range(len(current_dir)):
-        file_key = os.path.join(src, current_dir[i])
-        if os.path.isfile(file_key):
-            file_list.append(file_key)
-            _ = shutil.copy(file_key, target)
+        if os.path.isfile(src + "/" + current_dir[i]):
+            copy = shutil.copy(src + "/" + current_dir[i], target)
+            print(f"COPIED: \"{copy}\" into \"{target}\"")
             continue
-        os.mkdir(file_key.replace(src, target))
-        file_list.extend(copy_src(file_key, target))
-    return file_list
+        os.mkdir(target + "/" + current_dir[i])
+        copy_src(src + "/" + current_dir[i], target + "/" + current_dir[i])
+
